a65f05320f2a66f5dc5cabf8f505a0d17dcb81d5
Imran Sajeb <43618577+imransajeb@users.noreply.github.com>
2021-01-24 02:09:25 +0600

    add dev branch

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Fancy Lab functions ad definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fancy Lab
+ */
+
+functon fancy_lab_scripts(){
+	wp_enqueue_style( 'fancy-lab-style',   );
+}
+
