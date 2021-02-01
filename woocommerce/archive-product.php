d8789dfd56b243e68b85809775069abb42b0d8e3
Imran Sajeb <43618577+imransajeb@users.noreply.github.com>
2021-02-01 09:38:38 +0600

    some change

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -18,6 +18,23 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-3 col-md-4 order-2 order-md-1">
+			<?php
+
+				/**
+				 * Hook: woocommerce_sidebar.
+				 *
+				 * @hooked woocommerce_get_sidebar - 10
+				 */
+				do_action( 'woocommerce_sidebar' );
+			?>
+		</div>
+
+<div class="col-lg-9 col-md-8 order-1 order-md-2">
+<?php
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -26,9 +43,10 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-//do_action( 'woocommerce_before_main_content' );
+do_action( 'woocommerce_before_main_content' );
 
 ?>
+
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
@@ -95,11 +113,11 @@ if ( woocommerce_product_loop() ) {
  */
 do_action( 'woocommerce_after_main_content' );
 
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action( 'woocommerce_sidebar' );
+?>
+		</div>
+	</div>
+</div>
+<?php
+	get_footer( 'shop' );
+?>
 
-get_footer( 'shop' );
