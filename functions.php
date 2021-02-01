5c5bb033a7a30a493165b7ee62ab476b2ac43246
Imran Sajeb <43618577+imransajeb@users.noreply.github.com>
2021-02-01 08:46:30 +0600

    Add woodcommerce template

--- functions.php
+++ functions.php
@@ -32,9 +32,28 @@ add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
 function fancy_lab_config(){
 	register_nav_menus(
 		array(
-			'fancy_lab_main_menu'  		=> 'Fancy Lab Main Menu',
-			'fancy_lab_footer_menu'		=> 'Fancy Lab Footer Menu'
+			'fancy_lab_main_menu'  	=> 'Fancy Lab Main Menu',
+			'fancy_lab_footer_menu'	=> 'Fancy Lab Footer Menu'
 		)
 	); 
+
+	add_theme_support('woocommerce', array(
+		'thumbnail_image_width'	=> 255,
+		'single_image_width'	=> 255,
+		'product_grid'			=> array(
+			'default_rows'		=> 10,
+			'mix_rows'			=> 5,
+			'max_rows'			=> 10,
+			'default_columns'	=> 1,
+			'min_columns'		=> 1,
+			"max_columns"		=> 1,
+		)
+	));
+	add_theme_support('wc-product-gallery-zoom');
+	add_theme_support('wc-product-gallery-lightbox');
+	add_theme_support('wc-product-gallery-slider');
+	if(!isset( $content_width) ){
+		$content_width = 600;
+	}
 }
 add_action('after_setup_theme', 'fancy_lab_config', 0);
