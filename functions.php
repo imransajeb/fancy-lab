d038984aa11b949d4e3c3ae01b5b2a64ad203971
Imran Sajeb <43618577+imransajeb@users.noreply.github.com>
2021-02-01 21:29:34 +0600

    add wc-function file

--- functions.php
+++ functions.php
@@ -60,48 +60,5 @@ function fancy_lab_config(){
 
 add_action('after_setup_theme', 'fancy_lab_config', 0);
 
-/**
-* Modify WooCommerce opening and  closing HTML tags
-* We need Bootstrap-link opening/closeing HTML tags
-*/
-add_action('woocommerce_before_main_content', 'fancy_lab_open_container_row', 5);
-function fancy_lab_open_container_row(){
-	echo '<div class ="container shop-content"><div class="row">';
-}
 
-add_action('woocommerce_after_main_content', 'fancy_lab_close_container_row', 5 );
-function fancy_lab_close_container_row() {
-	echo '</div></div>';
-}
-
-add_action('woocommerce_before_main_content', 'fancy_lab_add_sidebar_tags', 6);
-function fancy_lab_add_sidebar_tags(){
-	echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
-}
-/**
-* Remove the main sidebar from its original option
-* We'll be including it somewhere else on
-*/
-
-remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar' );
-
-//Put the amin WC sidebar back , but using other action hook and on a different position
-
-add_action( 'woocommerce_before_main_content' , 'woocommerce_get_sidebar', 7);
-
-add_action ( 'woocommerce_before_main_content', 'fancy_lab_close_sidebar_tags', 8 );
-function fancy_lab_close_sidebar_tags(){
-	echo '</div>';
-}
-
-// Modify HTML tags on a shop page . we also want Bootstrap-link markup here (.primary div)
-
-add_action ( 'woocommerce_before_main_content', 'fancy_lab_add_shop_taggs', 9 );
-function fancy_lab_add_shop_taggs(){
-	echo '<div class= "col-lg-9 col-md-8 order-1 order-md-2">';
-}
-
-add_action( 'woocommerce_after_main_content', 'fancy_lab_close_shop_tags', 4 );
-function fancy_lab_close_shop_tags (){
-	echo '</div>';
-}
+require get_template_directory() . '/inc/wc-modifications.php';
